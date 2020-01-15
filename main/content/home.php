30356021e7b7a19e2f5ec4973b43b8ec9a7c8aa8
Toby2403 <42702195+Toby2403@users.noreply.github.com>
2020-01-15 11:21:30 +0100

    minimale Änderungen

--- main/content/home.php
+++ main/content/home.php
@@ -3,13 +3,26 @@
                
         $result = "";
 
-        $result .= '<body>
-                        <div id="allIncludedDiv" style="width:100%;height:100%;">';
+        $result .= '
+        <body>';
 
-                $result .= '<div id="wrappingContainer" class="container">';
-                                
-                $result .= '</div>
-                    </body>';
+            $result .= '
+            <div class="parent">
+                <div class="header">
+                        <div class="logIn">
+                            Einloggen
+                        </div>
+
+                        <div class="menu">
+                            Menü <img src="./images/dropdown.svg">
+                        </div>
+                </div>
+                <div class="davLogo"><img style="width:280px;" src="./images/davLogo.png"></div>
+                <div class="map">Hier die Map </div>
+            </div>'; 
+                           
+        $result .= '
+        </body>';
         
 
         return $result;
